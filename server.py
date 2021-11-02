5f9f72a625d7ae96cce0a79f8043f9e540a0aaa3
jacquetta <jacquettavb@hotmail.com>
2021-11-02 17:18:19 -0500

    added app route to test if base.html extends to homepage.html

--- server.py
+++ server.py
@@ -7,3 +7,19 @@ from jinja2 import StrictUndefined
 app = Flask(__name__)
 app.secret_key = "dev"
 app.jinja_env.undefined = StrictUndefined
+
+
+@app.route('/')
+def homepage():
+    return render_template('homepage.html')
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    connect_to_db(app)
+    app.run(host="0.0.0.0", debug=True)
