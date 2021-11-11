566feae6045791a580c31882cf7c6f07a50105fc
jacquetta <jacquettavb@hotmail.com>
2021-11-10 18:00:49 -0600

    added event listener to test

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,35 @@
-// ADD BUTTON 
-$('button.add-btn').on('click', () => {
-    alert("Testing")
-    
+// function addBook(evt){
+//     $.get('/volumes/<volume_id>', res =>{
+//         // $('#user-list').text(res);
+//         // let test = document.querySelector('.add-btn');
+//         test
+//         console.log(test);
+//     });
+// }
+// let book_list = []
+// function addBook(evt){
+// evt.preventDefault;
+//     volume_id = querySele
+//     console.log('testing');
+// }
+// $('.add-btn').on('click', addBook)
+
+// let book_list = [];
+// let btn;
+// $(document).ready(function() {
+// let listBook = document.getElementById('user-list');
+// })
+// $(document).ready(function() {
+//     $("button").click(function(evt) {
+//         // alert(evt.target.id);
+//         book_list.push(evt.target.id);
+//         $('ul').append(`<li>${evt.target.id}</li>`);
+//     });
+// });
+
+$('button').on('click', (evt) => {
+    let btn = evt.target.id;
+    console.log(btn)
 });
-// use id to append book to list 
+
+console.log(btn)
