e96a7cb23d68d6b720d1bec702b729297ea89191
jacquetta <jacquettavb@hotmail.com>
2021-11-17 16:25:38 -0600

    updated toggle feature on volume(book) to hide and show when button is clicked

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,39 @@
 // TOGGLE FOR MORE BUTTON IN BOOK LIST
-$(document).ready(function () {
-    $('.toggleBtn').click(function () {
-        $('#moreInfo').toggle();
-    });
-});
+
+// $('.toggleBtn').click(function (evt) {
+//     let btn = evt.target.id;
+//     btn.toggle();
+// });
+// let book_id = $('.moreInfo').attr('id');
+// let btn = $('.toggleBtn').attr('id');
+// $("#" + btn).click(function () {
+//     $("#" + book_id).toggle();
+// });
+
+// $('.toggleBtn').click( ()  => {
+//     let p = document.getElementsByClassName('moreInfo')
+//     let moreInfo = p.id;
+//     moreInfo.toggle();
+// });
+
+// $('.toggleBtn').toggle(getId);
+
+// $("toggleBtn").click(function(evt){
+//     let p = document.getElementsByClassName('moreInfo')
+//     let moreInfo = p.id;
+//     console.log(moreInfo)
+//     $("p").toggle();
+//   });
+
+
+// let book_id = $('.moreInfo').attr('id')
+// let btn_id = $('.toggleBtn').attr('id')
+
+function btnToggle(ID){
+    let book_id = document.getElementById(ID)
+    if(book_id.style.display === "none"){
+        book_id.style.display = "block";
+    } else {
+        book_id.style.display = "none";
+    }
+}
