ceaf99ac69fdbbe911322776a37ef83653c302d3
jacquetta <jacquettavb@hotmail.com>
2021-11-24 10:46:10 -0600

    added update btn event handler for profile update form

--- static/js/review_form.js
+++ static/js/review_form.js
@@ -16,15 +16,15 @@ $('#reviewBtn').on('click', () => {
     });
 });
 
-$('#reviewPost').on('click', () => {
+$('#updateBtn').on('submit', () => {
     // let reviewPosting = {
     //     volume_id: $('#volume_id').val(),
     //     title: $('#volume_title').val(),
     //     review_title: $('#reviewTitle').val(),
     //     review: $('#reviewPost').val(),
     // }
-    const formPost = $('#formPost').serialize(); 
-    $.post('/add_review', formPost)
+    const updateForm = $('#updateForm').serialize(); 
+    $.get('/update_profile', updateForm);
 
 
 });
