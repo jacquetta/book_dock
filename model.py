fc118352c132b385f9de13d39a2cd5d0ee0c85b5
jacquetta <jacquettavb@hotmail.com>
2021-11-25 13:38:27 -0600

    added test data

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 """Models for book tracking app"""
 
 from requests import NullHandler
+from crud import review
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import backref
 from datetime import datetime
@@ -79,6 +80,23 @@ class Review(db.Model):
 
     def __repr__(self):
         return f"<Review title={self.title} review_title={self.review_title} review={self.review} user={self.user_id}>"
+
+def test_data():
+    # if case is ran more than once, this will empty out data
+    User.query.delete()
+    Volume.query.delete()
+    Book_User.query.delete()
+    Review.query.delete()
+
+    # Test user, volume and review
+    test_user = User(user_id=22, full_name='Betty White', email='bw@email.com', password='Password123', book_goal=30, goal_date=2022-12-20)
+    test_volume = Volume(volume_id='bm-KDQAAQBAJ',title='Beloved', authors='Toni Morrison', genre='Fiction', summary='Spellbinding novel', published_date='2004', page_count=321, img_links='http://books.google.com/books/publisher/content?id=bm-KDQAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&imgtk=AFLRE73eDPxuPiW0zqjHk4H9xEj9N7obQzoAQ45A7tjU-yl2B0_th2uSt4J488Eg2mxsQ775cPPFg_P7uL-PibK1JQk7uX0rmt0BP_fQ0Tr-NM95IPpstWG5xeMVitJX78OFgSEkCZdf&source=gbs_api')
+    test_book_user = Book_User(volume_id='bm-KDQAAQBAJ', user_id=22)
+    test_user_review = Review(title='Beloved', review_title='Amazing', review='review', published='published', user_id=22, volume_id='bm-KDQAAQBAJ')
+
+    db.session.add_all([test_user, test_volume, test_book_user, test_user_review])
+    db.session.commit()
+
 
 
 
