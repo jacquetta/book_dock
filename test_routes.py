0a81e0000428b9ad89392fcee775908e67dde465
jacquetta <jacquettavb@hotmail.com>
2021-11-28 12:06:27 -0600

    testing setup complete

--- test_routes.py
+++ test_routes.py
@@ -25,15 +25,32 @@ class LoggedInTest(TestCase):
 
         result = self.client.get("/user_home")
         self.assertIn(b"Welcome to Book Dock!", result.data)
+    
+    def test_not_logged_in(self):
+        result = self.client.get('/', follow_redirects=True)
+        self.assertIn(b'Book Dock', result.data)
+
+
+
+class LandingPageTest(TestCase):
+    def setUp(self):
+        app.config['TESTING'] = True
+        self.client = app.test_client()
 
     def test_landing_page(self):
         result = self.client.get('/')
         self.assertEqual(result.status_code, 200)
         self.assertIn(b'Book Dock', result.data)
-    
-    def test_not_logged_in(self):
-        result = self.client.get('/', follow_redirects=True)
-        self.assertIn(b'Book Dock', result.data)
+
+class SignInTest(TestCase):
+    def setUp(self):
+        app.config['TESTING'] = True
+        self.client = app.test_client()
+
+    def test_signin_form(self):
+        result = self.client.post('/signin', data={'email': 'bw@email.com', 'password': 'Password123'}, follow_redirects=True)
+        self.assertIn(b'Welcome to Book Dock', result.data)
+
 
 class AppTest(TestCase):
     def setUp(self):
@@ -52,36 +69,10 @@ class AppTest(TestCase):
         db.drop_all()
         db.engine.dispose()
 
-    
-    # def test_signup_form(self):
-    #     result = self.client.post('/signup', data={'email'}, follow_redirects=True)
-    #     self.assertIn(b'Sign up to create your Book Dock Account', result.data)
 
-# class TestDataBase(TestCase):
 
-#     def setUp(self):
-#         self.client = app.test_client()
-#         app.config['TESTING'] = True
-#         connect_to_db(app, db_uri="postgresql:///testtracker")
-        
-#         db.create_all()
-#         test_data()
 
-#     def tearDown(self):
-#         db.session.remove()
-#         db.drop_all()
-#         db.engine.dispose()
 
-# class LogOutTest(TestCase):
-
-#     def setUp(self):
-#         app.config['TESTING'] = True
-#         self.client = app.test_client()
-    
-#     def test_user_page(self):
-#         result = self.client.get("/user_home", follow_redirects=True)
-#         self.assertNotIn(b"<h1>Welcome to Book Dock!</h1>", result.data)
-#         self.assertIn(b"Book Dock", result.data)
 
 if __name__ == '__main__':
     import unittest
