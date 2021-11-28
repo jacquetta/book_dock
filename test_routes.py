f0a019d1e5a4158dea916337f292b82726f1569b
jacquetta <jacquettavb@hotmail.com>
2021-11-28 12:20:38 -0600

    updated sign out test

--- test_routes.py
+++ test_routes.py
@@ -3,7 +3,7 @@ from unittest import TestCase
 from werkzeug import test
 
 from model import db, connect_to_db, test_data
-# from flask import session
+from flask import session
 from server import app
 from flask_sqlalchemy import SQLAlchemy
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///testtracker"
@@ -12,6 +12,7 @@ db = SQLAlchemy(app)
 
 
 class LoggedInTest(TestCase):
+
     def setUp(self):
         app.config['TESTING'] = True
         app.config['SECRET_KEY'] = 'key'
@@ -22,11 +23,12 @@ class LoggedInTest(TestCase):
                 session['key'] = 22
     
     def test_user_home(self):
-
+        """testing if user is logged in"""
         result = self.client.get("/user_home")
         self.assertIn(b"Welcome to Book Dock!", result.data)
     
     def test_not_logged_in(self):
+        """testing for user not logged in"""
         result = self.client.get('/', follow_redirects=True)
         self.assertIn(b'Book Dock', result.data)
 
@@ -38,6 +40,7 @@ class LandingPageTest(TestCase):
         self.client = app.test_client()
 
     def test_landing_page(self):
+        """testing for landing page"""
         result = self.client.get('/')
         self.assertEqual(result.status_code, 200)
         self.assertIn(b'Book Dock', result.data)
@@ -48,8 +51,22 @@ class SignInTest(TestCase):
         self.client = app.test_client()
 
     def test_signin_form(self):
-        result = self.client.post('/signin', data={'email': 'bw@email.com', 'password': 'Password123'}, follow_redirects=True)
+        """testing sign in form"""
+        with self.client as c:
+            result = c.post('/signin', data={'email': 'bw@email.com', 'password': 'Password123'}, follow_redirects=True)
         self.assertIn(b'Welcome to Book Dock', result.data)
+
+    def test_logout(self):
+        """testing log out route"""
+
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess['key'] = '22'
+            
+            result = self.client.get('/log_out', follow_redirects=True)
+
+            self.assertNotIn(b'user_id', session)
+            self.assertIn(b'Sign In', result.data)
 
 
 class AppTest(TestCase):
